fad3cefcb325b352bfb3f05443a2862a7b689413
Jacob Brockett <brockett04@gmail.com>
2026-02-17 18:13:21 -0600

    refactored app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
 import './App.css'
-import Project from "./pages/Project.tsx";
-import type {ProjectProps} from "./types/ProjectProps";
 import '@mantine/core/styles.css';
 import {MantineProvider} from "@mantine/core";
-import {useProjects} from "./hooks/useProjects.ts";
+import ProjectListPage from "./pages/ProjectListPage.tsx";
 
 function App() {
-    // TODO: REMOVE PLACEHOLDER:
-    const {projects, loading, error} = useProjects();
-    const project: ProjectProps = projects[0];
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error}</p>;
 
     return (
         <MantineProvider>
             <>
-                <Project
-                    title={project.title}
-                    tags={project.tags}
-                    description={project.description}
-                    relevantLinks={project.relevantLinks}
-                    demoVidLink={project.demoVidLink}
-                    srcCodeLink={project.srcCodeLink}
-                />
+                <ProjectListPage/>
             </>
         </MantineProvider>
     )
 }
 
-export default App
+export default App;
